a508963f2f61a8dc7e54ee51fa837b9590093340
Mondal10 <amitmondal111.am@gmail.com>
2022-09-03 17:01:47 +0530

    repo setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      Hello
+      <h1 className="text-3xl font-bold underline text-blue-400">
+        Hello world! 'hello' "ok"
+      </h1>
+    </div>
+  );
+}
+
+export default App;
