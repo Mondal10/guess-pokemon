0ef19c3d7801b60c35e38320b747ee8434f34386
Mondal10 <amitmondal111.am@gmail.com>
2022-10-09 13:12:00 +0530

    add: pokemon type info

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,6 +1,7 @@
 import pokemonHeaderImg from "@/assets/images/pokemonHeader.png";
 import Button from "@/components/atoms/Button";
 import { ROUTE_PATH } from "@/routes/enums";
+import { POKEMON_TYPE_COLOUR } from "@/shared/constants";
 import { getGenerationRange, getRandomNumber } from "@/shared/utils";
 import { getSelectedGenerations } from "@/store/features/game/gameSelector";
 import { gameSliceActions } from "@/store/features/game/gameSlice";
@@ -41,13 +42,29 @@ function Landing() {
       <div className="w-5/6 sm:w-2/3 mt-10">
         <p>
           A random Pokémon will appear based on generations selected from
-          settings(initially all generations are selected by default), can you
-          spell its name correctly? You'll have 6 attemps to do that.
+          settings(initially only Gen 1 pokémons are selected by default), can
+          you spell them all correctly? You'll have 6 attemps to do that.
         </p>
         <p className="mt-4">
           Want to make it more challenging? Go to settings and change the
           difficulty.
         </p>
+        <h2 className="text-lg font-medium mt-10">Pokemon Type Indicator:</h2>
+        <p>Pokemon background will blink with this colour</p>
+        <div className="flex justify-evenly capitalize">
+          {Object.keys(POKEMON_TYPE_COLOUR).map((pokemonType) => (
+            <div className="flex items-center">
+              <span
+                className={`${
+                  POKEMON_TYPE_COLOUR[
+                    pokemonType as keyof typeof POKEMON_TYPE_COLOUR
+                  ]
+                } inline-block opacity-75 w-4 h-4 rounded-full m-2`}
+              ></span>
+              <span className="capitalize">{pokemonType}</span>
+            </div>
+          ))}
+        </div>
         <h2 className="text-lg font-medium mt-10">How To Play?</h2>
         <ul className="list-disc list-inside">
           <li>
